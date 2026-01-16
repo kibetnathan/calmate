b1570ef6a4c55d1698343efaf6a8e4485431bdf2
kibetnathan <kibetnat@gmail.com>
2026-01-16 09:18:10 +0300

    add dashboard, create static folder

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.conf import settings
+from django.db import models
 
 # Create your models here.
+
+class FoodEntry(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=100)
+    calories = models.PositiveIntegerField(help_text="kcal")
+
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.calories} kcal)"
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     context = {"message" : "Welcome to Django!"}
     return render(request, 'index.html')
+
+@login_required
+def dashboard(request):
+    return render(request, "dashboard.html")
